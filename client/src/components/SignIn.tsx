f34b161cf9ea6353c5df7731b6dbc703eac5264c
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-03 12:01:49 +0200

    shows current user

--- client/src/components/SignIn.tsx
+++ client/src/components/SignIn.tsx
@@ -1,22 +1,33 @@
 import { Button, Box } from '@chakra-ui/react';
 import { useState } from 'react';
 import { getFirebase } from '../firebase';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 const { auth } = getFirebase();
 
 export default function SignIn(): any {
+  const [currentUserEmail, setCurrentUserEmail] = useState('');
+
   const signInWIthGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
   };
+
+  onAuthStateChanged(auth, (user: any) => {
+    setCurrentUserEmail(user.email);
+  });
 
   return (
     <Box>
       <Button colorScheme='blue' onClick={signInWIthGoogle}>
         Sign in with Google
       </Button>
-      <div>Current User</div>
+      <p>Current User</p>
+      <p>{currentUserEmail}</p>
     </Box>
   );
 }
