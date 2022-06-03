eb040a6d9e1181eef3d67ec792f21f709b91ebd5
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-03 09:56:17 +0200

    config file added

---
+++ client/src/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+  firebase: {
+    apiKey: 'AIzaSyDznGU_LNiEqWRP1lDiraJi7BVlTZ_zXcg',
+    authDomain: 'buzzlokk-01.firebaseapp.com',
+    projectId: 'buzzlokk-01',
+    storageBucket: 'buzzlokk-01.appspot.com',
+    messagingSenderId: '397709881433',
+    appId: '1:397709881433:web:0cf5f87d9536df3cc78148',
+    measurementId: 'G-L7BX1Z0RRY',
+  },
+};
