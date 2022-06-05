5e1525d57cf1975b90894660cb60f4f18482bb5c
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-05 10:09:02 +0200

    persisting data in emulator fixed

--- client/src/components/EventDetails.tsx
+++ client/src/components/EventDetails.tsx
@@ -1,12 +1,24 @@
 import { FC } from 'react';
-import { eve } from '../types/types';
+import { eve, match } from '../types/types';
+import { Box, Text, VStack } from '@chakra-ui/react';
+import Match from '../components/Match';
 
 interface Props {
   currentEvent: eve | undefined;
 }
 
 const EventDetails: FC<Props> = ({ currentEvent }) => {
-  return <div>{currentEvent && <div>{currentEvent?.title}</div>}</div>;
+  return (
+    <Box mx='auto'>
+      {currentEvent && <Text fontSize='3xl'>{currentEvent?.title}</Text>}
+      <VStack>
+        {currentEvent?.matches &&
+          currentEvent?.matches.map((match: match) => {
+            return <Match match={match} />;
+          })}
+      </VStack>
+    </Box>
+  );
 };
 
 export default EventDetails;
